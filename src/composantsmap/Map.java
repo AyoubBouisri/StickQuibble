4e4d2619a752e8f7387c9abcbb7688eeb83c12c4
Andy <andy.lam912@gmail.com>
2018-12-26 20:58:44 -0500

    Debut Class Map

--- src/composantsmap/Map.java
+++ src/composantsmap/Map.java
@@ -1,17 +1,24 @@
 package composantsmap;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+
 
 public class Map {
 	private static int WIDTH_MAP = 4000,HEIGHT_MAP = 2500; 
 	
 	private int xCamera,yCamera;
-	
+	private ArrayList<Plateforme> listePlateforme;
 	public Map() {
-		
+		// TEST SEULEMENT A CHANGER
+		listePlateforme.add(new Plateforme(0, 0, 500, 500));
 	}
 	public void dessiner(Graphics2D g2d) {
-	
+		
+		for(int i = 0 ;i < listePlateforme.size(); i++) {
+			listePlateforme.get(i).dessiner(g2d);
+		}
+		
 	}
 	
 	public void scaleCamera() {
@@ -21,4 +28,5 @@ public class Map {
 	public void moveCamera() {
 		
 	}
+	
 }
