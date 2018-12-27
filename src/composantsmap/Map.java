96a4a6f64a6040ec5188463df1c211585ba0034f
AyoubBouisri <ayoubbouisri@hotmail.fr>
2018-12-26 21:57:16 -0500

    Dessiner Plateforme dans ecran

--- src/composantsmap/Map.java
+++ src/composantsmap/Map.java
@@ -13,6 +13,7 @@ public class Map {
 	private Vecteur vecCamera = new Vecteur(0,0);
 	private int widthCamera = 1300, heightCamera = 700;
 	private ArrayList<Plateforme> listePlateforme = new ArrayList<Plateforme>();
+	
 	public Map(ArrayList<Plateforme> listePlateforme) {
 		this.listePlateforme = listePlateforme;
 
@@ -21,7 +22,7 @@ public class Map {
 		
 		for(int i = 0 ;i < listePlateforme.size(); i++) {
 			if(isInMap(listePlateforme.get(i))) {
-				listePlateforme.get(i).dessiner(g2d);
+				listePlateforme.get(i).dessinerDansEcran(g2d);
 			}
 		}
 		
@@ -34,6 +35,7 @@ public class Map {
 	public void moveCamera() {
 		
 	}
+	
 	public boolean isInMap(Plateforme plateforme) {
 		if(123 - vecCamera.getX()  > 0 &&  123 -vecCamera.getX()+ widthCamera < 0 && 321 - vecCamera.getY() < 0 && 321 - vecCamera.getY() + heightCamera >0) {
 			return true;
--- src/composantsmap/Plateforme.java
+++ src/composantsmap/Plateforme.java
@@ -1,22 +1,37 @@
 package composantsmap;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 
+import geometrie.Vecteur;
+
 public class Plateforme {
-	private int x,y;
+	public Vecteur  position = new Vecteur();
 	private int width,height;
 	
 	private Rectangle2D.Double shapePlateforme;
+	
 	public Plateforme(int x,int y, int width,int height) {
-		this.x = x;
-		this.y = y;
+		position.setX(x);
+		position.setY(y);
 		this.width = width;
 		this.height = height;
 		
 		shapePlateforme = new Rectangle2D.Double(x,y,width,height);
 	}
-	public void dessiner(Graphics2D g2d) {
+	
+		
+	public void dessinerDansEcran(Graphics2D g2d, Vecteur camera) {
+	
+		double xEcran = position.getX() - camera.getX();
+		double yEcran = position.getY() - camera.getY();
+		shapePlateforme.setRect(xEcran, yEcran, width, height);
+		g2d.setColor(new Color(34,177,76));
 		g2d.fill(shapePlateforme);
+		
+		
 	}
+	
+	
 }
