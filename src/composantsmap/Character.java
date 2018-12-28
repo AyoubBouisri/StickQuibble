fccd0b4402536106bc67e153c907275bcd011594
Andy <andy.lam912@gmail.com>
2018-12-28 01:59:21 -0500

    Beginning to add jumping mechanics

--- src/composantsmap/Character.java
+++ src/composantsmap/Character.java
@@ -7,16 +7,25 @@ import java.awt.geom.Ellipse2D;
 import geometrie.Vecteur;
 
 public class Character {
+	public final static int JUMP_HEIGHT = 50;
+	public final static int MAX_SPEED_DOWN = 30;
+	public final static int ACCEL_Y = -30;
+	public final static int JUMP_SPEED = 30;
+	
 	public int damage;
-	public int speed;
+	public Vecteur speed;;
 	public Ellipse2D.Double hitBox;
 	public int width , height;
 	public Vecteur position;
+	public int curJumpCount = 0;
+
 	
 	public Character(Vecteur position, int width, int height) {
 		this.position = position;
 		this.width = width;
 		this.height = height;
+		speed = new Vecteur();
+		damage =0;
 		hitBox = new Ellipse2D.Double(position.getX(), position.getY(), width, height);
 	}
 	
@@ -26,6 +35,13 @@ public class Character {
 		hitBox.setFrame(xEcran, yEcran, width, height);
 		g2d.fill(hitBox);
 	}
+	public void jump() {
+		speed.setY(30);
+	}
+	public boolean canJump() {
+		return (curJumpCount==1 || curJumpCount==0);
+	}
+	
 	
 	
 
